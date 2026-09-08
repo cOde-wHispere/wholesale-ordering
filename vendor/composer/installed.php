9ac061c7046f1a4f9482a4d682fba7888218e332
cOde-wHispere <demba.b.jallow@gmail.com>
2026-09-08 11:16:54 +0000

    build: include production Composer dependencies

---
+++ vendor/composer/installed.php
@@ -0,0 +1,23 @@
+<?php return array(
+    'root' => array(
+        'name' => 'code-whispere/wholesale-ordering',
+        'pretty_version' => 'dev-main',
+        'version' => 'dev-main',
+        'reference' => '1971cc493a8dd208828c27bc46da8b24d71fbe58',
+        'type' => 'wordpress-plugin',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'dev' => false,
+    ),
+    'versions' => array(
+        'code-whispere/wholesale-ordering' => array(
+            'pretty_version' => 'dev-main',
+            'version' => 'dev-main',
+            'reference' => '1971cc493a8dd208828c27bc46da8b24d71fbe58',
+            'type' => 'wordpress-plugin',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+    ),
+);
